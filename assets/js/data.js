8a29543a9e9bf391a4867845e2e5ba3f795d7194
Anthony GARGOUA <56581894+AnthonyGargoua@users.noreply.github.com>
2026-04-28 14:21:24 +0200

    data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,1 +1,30 @@
-
+const initialProducts = [
+    { 
+        id: 1, 
+        name: "Hoodie Shinigami V1", 
+        price: 65, 
+        category: 'hoodie', 
+        image: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=800" 
+    },
+    { 
+        id: 2, 
+        name: "Cargo 'Tech-Kyoto'", 
+        price: 85, 
+        category: 'pant', 
+        image: "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?q=80&w=800" 
+    },
+    { 
+        id: 3, 
+        name: "Kimono Black Mesh", 
+        price: 120, 
+        category: 'hoodie', 
+        image: "https://images.unsplash.com/photo-1578301978018-3005759f48f7?q=80&w=800" 
+    },
+    { 
+        id: 4, 
+        name: "Tee Oversize 'Akira'", 
+        price: 35, 
+        category: 'tee', 
+        image: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=800" 
+    }
+];
